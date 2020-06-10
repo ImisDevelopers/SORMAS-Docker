453d7877d8f00a9260ddcff7a815c6a9481b0f67
Jonas Cirotzki <s8jociro@stud.uni-saarland.de>
2020-06-10 17:07:17 +0200

    fix python

--- scratch_space/sormas-api.py
+++ scratch_space/sormas-api.py
@@ -1,29 +1,47 @@
 import json
 from datetime import datetime
 from uuid import uuid4
-
 import requests
 
-rest = "https://sormas-docker-test.com/sormas-rest/"
 
-s = requests.Session()
-s.verify = False
-s.auth = ('SurvOff', 'SurvOff')
+def gen_entity_dto():
+    now = '{}'.format(datetime.utcnow().replace(microsecond=0).isoformat())
+    uuid = '{}'.format(uuid4())
 
-now = '%s'.format(datetime.utcnow())
-uuid = '%s'.format(uuid4())
-
-payload = [
-    {
+    return {
         "creationDate": now,
         "changeDate": now,
         "uuid": uuid,
-        "firstName": "FirstName",
-        "lastName": "LastName"
     }
-]
 
-p = s.post(rest + '/persons/push', json=json.dumps(payload))
-print(p.text)
-g = s.get(rest + '/persons/uuids')
-print(g.text)
+
+def main():
+    rest = "https://sormas-docker-test.com/sormas-rest/"
+
+    s = requests.Session()
+    s.verify = False
+    s.auth = ('SurvOff', 'SurvOff')
+
+    person = gen_entity_dto()
+    person["firstName"] = "first2"
+    person["lastName"] = "last2"
+
+    payload = [person]
+
+    p = s.post(rest + '/persons/push', json=payload)
+    print(p.text)
+
+    # g = s.get(rest + '/persons/uuids')
+    # print(g.status_code)
+
+    # q = s.post(rest + '/persons/query', json=[uuid])
+    # print(q.text)
+
+    with open('example-case.json', 'rb') as f:
+        payload = json.load(f)
+
+    c = s.post(rest + '/cases/push', json=payload)
+    print(c.text)
+
+if __name__ == '__main__':
+    main()
